c7b29527e1360df15e3e79c87b84a39dbaae2040
gonzalo <gonzalo_a_fuente@hotmail.com>
2022-06-16 23:03:27 -0300

    Bash script finished

--- startapp.sh
+++ startapp.sh
@@ -3,3 +3,9 @@
 #set -e
 
 #Run the Frontend
+git clone https://github.com/camilapensolvers/GitHubgonzalo-fuente-Ensolvers-challange.git Awesome_Notes
+cd Awesome_Notes
+yarn install
+yarn start
+
+
