bc4b393b7ea3033f84fadb728d1a3aa0a70572ea
gonzalo <gonzalo_a_fuente@hotmail.com>
2022-07-16 00:59:19 -0300

    Fix: bug in DB seed fixed

--- Backend/seeders/20220716020649-user.js
+++ Backend/seeders/20220716020649-user.js
@@ -5,7 +5,8 @@ module.exports = {
     return queryInterface.bulkInsert("Users", [
       {
         email: "user@user.com",
-        password: "password",
+        password:
+          "$2b$10$dILi9b4h3vPNVXdmZ1N5K.WT3Z4I3dEeJrWZO9x6Z2AcRqVpOzis6",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
